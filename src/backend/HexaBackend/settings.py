d09b1cf3578538ff334bca3da7efa42479e71c4c
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2022-07-02 21:44:27 +0530

    should ve been pushed in march

--- src/backend/HexaBackend/settings.py
+++ src/backend/HexaBackend/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import imp
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from hexaBackend import finalzed_model
 
+import pickle
+loaded_model = pickle.load(BASE_DIR/finalzed_model, 'rb')()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
