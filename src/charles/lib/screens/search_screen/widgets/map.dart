3cd32cc9f1aa3549769da825fba30765a6714c16
Poujhit <poujhit.dev@gmail.com>
2022-08-25 17:38:57 +0530

    Seaarch changes

---
+++ src/charles/lib/screens/search_screen/widgets/map.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapWidget extends StatefulWidget {
+  const MapWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(),
+      layers: [
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              point: LatLng(30, 40),
+              width: 80,
+              height: 80,
+              builder: (context) => FlutterLogo(),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
