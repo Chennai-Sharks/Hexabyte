57e9cf2533a8c0ebc0d94399c63884fef69e043c
Poujhit <poujhit.dev@gmail.com>
2022-08-26 02:00:18 +0530

    Form structure

--- src/charles/lib/screens/add_product_screen/config/form_structure.dart
+++ src/charles/lib/screens/add_product_screen/config/form_structure.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 final List<Map<String, dynamic>> addProductFormStructure = [
   {
     'type': 'text',
-    'name': 'title',
+    'name': 'food_waste_title',
     'label': 'Enter title',
     'required': true,
     'keyboardType': TextInputType.name,
@@ -11,9 +11,9 @@ final List<Map<String, dynamic>> addProductFormStructure = [
   {
     'type': 'text',
     'name': 'duration',
-    'label': 'Enter duration(no. of months)',
+    'label': 'Enter no. of days ',
     'required': true,
-    'keyboardType': TextInputType.number
+    'keyboardType': TextInputType.number,
   },
   {
     'type': 'text',
@@ -32,7 +32,7 @@ final List<Map<String, dynamic>> addProductFormStructure = [
   },
   {
     'type': 'multichip',
-    'name': 'tags',
+    'name': 'applicable_tags',
     'label': 'Select Product tags',
     'required': true,
     'values': [
